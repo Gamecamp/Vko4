e076778bb488c9a2f3aaf7f83b6bfa442fd33362
Flyri <simo.larvi@cs.tamk.fi>
2016-07-19 16:09:54 +0300

    Attack combo kesken, animaatiot myös

--- NotSuperSmash/Assets/Scripts/General Character Scripts/PlayerAttackManager.cs
+++ NotSuperSmash/Assets/Scripts/General Character Scripts/PlayerAttackManager.cs
@@ -19,13 +19,23 @@ public class PlayerAttackManager : MonoBehaviour {
 
 	private float chainResetTime;
 
-	private int numberInAttackChain;
-	private int maxChain;
+	private int currentCombo;
+	private int maxCombo;
 	private int attackPhaseHelper;
 
 	private bool attackInProgress;
 
 	private string activeWeapon;
+
+	private enum AttackState {idlePhase, startPhase, startAnimationPhase, damagePhase, comboPhase, recoveryPhase};
+	private AttackState attackPhase;
+	private bool isAttackHitbox;
+	private bool Action1Input;
+	private bool Action2Input;
+
+	private string animationComboOdd;
+	private string animationComboEven;
+	private string animationComboFinal;
 
 	const string unarmed = "unarmed";
 	const string baseballBat = "baseballBat";
@@ -41,10 +51,12 @@ public class PlayerAttackManager : MonoBehaviour {
 
 		activeWeapon = unarmed;
 
-		numberInAttackChain = 1;
+		currentCombo = 1;
 
 		attackPhaseHelper = 0;
 		attackDuration = 0;
+
+		attackPhase = AttackState.startPhase;
 	}
 
 	// Update is called once per frame
@@ -54,90 +66,147 @@ public class PlayerAttackManager : MonoBehaviour {
 
 	void UpdateAttacking () {
 
-		if (player.GetIsAction1Input() && player.GetCanInputActions() && !attackInProgress) {
-			player.SetIsLightAttacking (true);
-			DetermineAttackProperties();
-			attackInProgress = true;
-		} else if (player.GetIsAction2Input() && player.GetCanInputActions() && !attackInProgress) {
-			player.SetIsHeavyAttacking (true);
-			DetermineAttackProperties();
-			attackInProgress = true;
+		if (player.GetIsAction1Input () && player.GetCanInputActions ()) {
+			Action1Input = true;
+		} else if (player.GetIsAction2Input () && player.GetCanInputActions ()) {
+			Action2Input = true;
+		}
+			
+		if (attackPhase == AttackState.startPhase) {
+			if (Action1Input) {
+				Action1Input = false;
+				player.SetIsLightAttacking (true);
+				DetermineAttackProperties ();
+				DetermineRightAnimation ();
+			} else if (Action2Input) {
+				Action2Input = false;
+				player.SetIsHeavyAttacking (true);
+				DetermineAttackProperties ();
+				//anim.start/play( DetermineRightAnimation (hitboxUsedInAttack.name) , true);
+			}
+		} else if (attackPhase == AttackState.damagePhase) {
+			hitboxUsedInAttack.SetActive (true);
+		} else if (attackPhase == AttackState.comboPhase) {
+			hitboxUsedInAttack.SetActive (false);
+			ComboHit ();
+		} else if (attackPhase == AttackState.recoveryPhase) {
+			ResetAttack ();
+			ResetAttackChain ();
+		}
+			
+//		if (attackPhase == AttackState.setUpPhase) {
+//			if (player.GetIsAction1Input () && player.GetCanInputActions () && !attackInProgress) {
+//				player.SetIsLightAttacking (true);
+//				DetermineAttackProperties ();
+//				attackInProgress = true;
+//			} else if (player.GetIsAction2Input () && player.GetCanInputActions () && !attackInProgress) {
+//				player.SetIsHeavyAttacking (true);
+//				DetermineAttackProperties ();
+//				attackInProgress = true;
+//			}
+//		}
+//
+//		if (player.GetIsLightAttacking () || player.GetIsHeavyAttacking ()) {
+//
+//			//  Attack doesn't hurt yet, swing windup time
+//			attackDuration = attackDuration + Time.deltaTime;
+//
+//			// Attack hurtbox goes on
+//			if (attackDuration >= beforeHurtAnimationLength && attackPhaseHelper == 0) {
+//				hitboxUsedInAttack.SetActive (true);
+//				attackPhaseHelper++;
+//			} 
+//
+//			// Attack has been delivered, still can't move, recovery starts
+//			if (attackDuration >= beforeHurtAnimationLength + hurtfulAnimationLength && attackPhaseHelper == 1) {
+//				hitboxUsedInAttack.SetActive (false);
+//				if (maxChain > 1) {
+//					if (numberInAttackChain < maxChain) {
+//						numberInAttackChain++;
+//					} else {
+//						ResetAttackChain ();
+//					}
+//				}
+//				attackPhaseHelper++;
+//			}
+//
+//			// After recovery control is given back to the player
+//			if (attackDuration >= beforeHurtAnimationLength + hurtfulAnimationLength + recoveryTime && attackPhaseHelper == 2) {
+//				ResetAttack ();
+//			}
+//		} else if (attackInProgress) {
+//			ResetAttack ();
+//		}
+	}
+
+	string DetermineRightAnimation(string animationPrefix) {
+		string anim = animationPrefix.Substring (0, animationPrefix.Length - 7);
+
+		if (currentCombo == maxCombo) {
+			anim = animationComboFinal;
+		} else if (currentCombo % 2 != 0) {
+			//odd
+			anim = animationComboOdd;
+		} else {
+			//even
+			anim = animationComboEven;
 		}
 
-		if (player.GetIsLightAttacking () || player.GetIsHeavyAttacking ()) {
+		return anim;
+	}
 
-			//  Attack doesn't hurt yet, swing windup time
-			attackDuration = attackDuration + Time.deltaTime;
-
-			// Attack hurtbox goes on
-			if (attackDuration >= beforeHurtAnimationLength && attackPhaseHelper == 0) {
-				hitboxUsedInAttack.SetActive (true);
-				attackPhaseHelper++;
-			} 
-
-			// Attack has been delivered, still can't move, recovery starts
-			if (attackDuration >= beforeHurtAnimationLength + hurtfulAnimationLength && attackPhaseHelper == 1) {
-				hitboxUsedInAttack.SetActive (false);
-				if (maxChain > 1) {
-					if (numberInAttackChain < maxChain) {
-						numberInAttackChain++;
-					} else {
-						ResetAttackChain ();
-					}
-				}
-				attackPhaseHelper++;
-			}
-
-			// After recovery control is given back to the player
-			if (attackDuration >= beforeHurtAnimationLength + hurtfulAnimationLength + recoveryTime && attackPhaseHelper == 2) {
-				ResetAttack ();
-			}
-		} else if (attackInProgress) {
-			ResetAttack ();
+	void ComboHit() {
+		if((currentCombo != maxCombo) && (Action1Input || Action2Input)) {
+			attackPhase = AttackState.startPhase;
+			currentCombo++;
 		}
 	}
 
 	void DetermineAttackProperties() {
 		switch (activeWeapon) {
-		case unarmed:
-			if (player.GetIsLightAttacking()) {
-				hitboxUsedInAttack = unarmedLightHitbox;
+			case unarmed:
+				if (player.GetIsLightAttacking()) {
+					hitboxUsedInAttack = unarmedLightHitbox;
 
-				beforeHurtAnimationLength = 0.1f;
-				hurtfulAnimationLength = 0.1f;
-				recoveryTime = 0.05f;
+					beforeHurtAnimationLength = 0.1f;
+					hurtfulAnimationLength = 0.1f;
+					recoveryTime = 0.05f;
 
-				maxChain = 3;
-			} else {
-				hitboxUsedInAttack = unarmedHeavyHitbox;
+					maxCombo = 3;
+				} else {
+					hitboxUsedInAttack = unarmedHeavyHitbox;
 
-				beforeHurtAnimationLength = 0.2f;
-				hurtfulAnimationLength = 0.2f;
-				recoveryTime = 0.1f;
+					beforeHurtAnimationLength = 0.2f;
+					hurtfulAnimationLength = 0.2f;
+					recoveryTime = 0.1f;
 
-				maxChain = 1;
-			}
-			break;
-		case baseballBat:
-			if (player.GetIsLightAttacking()) {
-				hitboxUsedInAttack = baseballBatLightHitbox;
+					maxCombo = 1;
+				}
+				break;
+			case baseballBat:
+				if (player.GetIsLightAttacking()) {
+					hitboxUsedInAttack = baseballBatLightHitbox;
 
-				beforeHurtAnimationLength = 0.3f;
-				hurtfulAnimationLength = 0.4f;
-				recoveryTime = 0.5f;
+					beforeHurtAnimationLength = 0.3f;
+					hurtfulAnimationLength = 0.4f;
+					recoveryTime = 0.5f;
 
-				maxChain = 3;
-			} else {
-				hitboxUsedInAttack = baseballBatHeavyHitbox;
+					maxCombo = 3;
+				} else {
+					hitboxUsedInAttack = baseballBatHeavyHitbox;
 
-				beforeHurtAnimationLength = 0.7f;
-				hurtfulAnimationLength = 0.8f;
-				recoveryTime = 0.9f;
+					beforeHurtAnimationLength = 0.7f;
+					hurtfulAnimationLength = 0.8f;
+					recoveryTime = 0.9f;
 
-				maxChain = 1;
-			}
-			break;
+					maxCombo = 1;
+				}
+				break;
 		}
+	}
+
+	public void ChangeAttackPhase(AttackState attackPhase) {
+		this.attackPhase == attackPhase;
 	}
 
 	void ResetAttack() {
@@ -150,7 +219,7 @@ public class PlayerAttackManager : MonoBehaviour {
 	}
 
 	void ResetAttackChain() {
-		numberInAttackChain = 1;
+		currentCombo = 1;
 	}
 
 	public void SetActiveWeapon(string weapon) {
