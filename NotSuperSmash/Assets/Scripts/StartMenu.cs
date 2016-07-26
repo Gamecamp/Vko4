81ec66580233570c604bb4dfa233810316f3624d
banbanxaixai <anton.peltoniemi@gmail.com>
2016-07-26 17:46:05 +0300

    UI animations
    UI animations

---
+++ NotSuperSmash/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public Animator pressAnyButtonAnimator;
+	public Animator playerSelectionAnimator;
+
+	private bool startUpMode;
+	private bool playerSelectionMode;
+
+	// Use this for initialization
+	void Start () {
+		startUpMode = true;
+		playerSelectionMode = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startUpMode && Input.anyKeyDown) {
+			StartPlayerSelectionMode ();
+		}
+	}
+
+	void StartPlayerSelectionMode() {
+		startUpMode = false;
+		playerSelectionMode = true;
+		pressAnyButtonAnimator.SetBool ("buttonPressed", true);
+		playerSelectionAnimator.SetBool ("slideIn", true);
+	}
+}
